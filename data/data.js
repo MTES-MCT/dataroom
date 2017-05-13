721265e04a3a4e95ebd8c3d6596d7ac91015685c
ouhouhsami <samuel.goldszmidt@gmail.com>
2017-05-13 15:05:13 +0200

    clean structure and some code

--- data/data.js
+++ data/data.js
@@ -1,17 +1,13 @@
-
-var myurljson = "https://meem-mlhd.github.io/dataroom/data/data.json" ;
-//var myurljson = "data/data.json" ;
-
 var mydata = (function () {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': myurljson,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-        }
-    });
-    return json;
-})(); 
+  var json = null;
+  $.ajax({
+    'async': false,
+    'global': false,
+    'url': "./data/data.json",
+    'dataType': "json",
+    'success': function (data) {
+      json = data;
+    }
+  });
+  return json;
+})();
